ba4d78e1729ce133b2264211044d8eaf8c9ac6f7
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2024-09-25 15:00:26 +0700

    df

--- packages/leazycms/src/views/backend/posts/custom_field/text.blade.php
+++ packages/leazycms/src/views/backend/posts/custom_field/text.blade.php
@@ -1,3 +1,3 @@
 <small for="{{_us($r[0])}}">{{$r[0]}}</small>
-<input {{ isset($r[2]) ? 'required':'' }} type="text" value="@if($post->user->isOperator() && ( $r[0]=='Instansi' ||  $r[0]=='Alamat') && isset($field[_us($r[0])]) && empty($field[_us($r[0])])) @if($r[0]=='Instansi') {{ $post->user->unit->parent->title }} @else {{ $post->user->unit->parent->data_field['alamat'] }} @endif @else {{ isset($field[_us($r[0])]) ? trim($field[_us($r[0])]) :''}}@endif"
+<input {{ isset($r[2]) ? 'required':'' }} type="text" value="@if($post->user->isOperator() && ($r[0]=='Instansi' ||  $r[0]=='Alamat') && isset($field[_us($r[0])]) && empty($field[_us($r[0])])) @if($r[0]=='Instansi') {{ $post->user->unit->parent->title }}@else{{ $post->user->unit->parent->data_field['alamat'] }} @endif @else {{ isset($field[_us($r[0])]) ? trim($field[_us($r[0])]) :null}}@endif"
  class="form-control form-control-sm" name="{{_us($r[0])}}" placeholder="Entri {{$r[0]}}">
