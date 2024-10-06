8e58596794ce32146355b09c4d35c4350eb5031b
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2024-10-06 14:57:13 +0700

    update

--- packages/leazycms/src/routes/web.php
+++ packages/leazycms/src/routes/web.php
@@ -6,7 +6,7 @@ use Leazycms\Web\Http\Controllers\SuratController;
 Route::match(['post'],'cek_surat', [SuratController::class, 'cek_surat'])->middleware('auth');
 Route::match(['post'],'upload_docx', [SuratController::class, 'upload_docx'])->middleware('auth');
 Route::match(['get', 'post'],'surat-keluar/{keyword}', [SuratController::class, 'index']);
-Route::match(['get', 'post'],'qr_surat/{keyword}', [SuratController::class, 'qr_surat'])->middleware('auth');
+Route::match(['get', 'post'],'qr_surat/{keyword}', [SuratController::class, 'qr_surat']);
 Route::match(['get', 'post'],'generate/{keyword}', [SuratController::class, 'generate_surat'])->middleware('auth');
 
 
